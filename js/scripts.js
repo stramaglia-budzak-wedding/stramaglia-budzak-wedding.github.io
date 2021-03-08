60f5eaf85511b080fca30fff98687f2341afd162
stramaglia-budzak-wedding <78329315+stramaglia-budzak-wedding@users.noreply.github.com>
2021-03-07 23:04:31 -0600

    initial commit
    putting up the first version of the page!

---
+++ js/scripts.js
@@ -0,0 +1,206 @@
+$(document).ready(function () {
+
+    /***************** Waypoints ******************/
+
+    $('.wp1').waypoint(function () {
+        $('.wp1').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp2').waypoint(function () {
+        $('.wp2').addClass('animated fadeInRight');
+    }, {
+        offset: '75%'
+    });
+    $('.wp3').waypoint(function () {
+        $('.wp3').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp4').waypoint(function () {
+        $('.wp4').addClass('animated fadeInRight');
+    }, {
+        offset: '75%'
+    });
+    $('.wp5').waypoint(function () {
+        $('.wp5').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp6').waypoint(function () {
+        $('.wp6').addClass('animated fadeInRight');
+    }, {
+        offset: '75%'
+    });
+    $('.wp7').waypoint(function () {
+        $('.wp7').addClass('animated fadeInUp');
+    }, {
+        offset: '75%'
+    });
+    $('.wp8').waypoint(function () {
+        $('.wp8').addClass('animated fadeInLeft');
+    }, {
+        offset: '75%'
+    });
+    $('.wp9').waypoint(function () {
+        $('.wp9').addClass('animated fadeInRight');
+    }, {
+        offset: '75%'
+    });
+
+    /***************** Initiate Flexslider ******************/
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+
+    /***************** Initiate Fancybox ******************/
+
+    $('.single_image').fancybox({
+        padding: 4
+    });
+
+    $('.fancybox').fancybox({
+        padding: 4,
+        width: 1000,
+        height: 800
+    });
+
+    /***************** Tooltips ******************/
+    $('[data-toggle="tooltip"]').tooltip();
+
+    /***************** Nav Transformicon ******************/
+
+    /* When user clicks the Icon */
+    $('.nav-toggle').click(function () {
+        $(this).toggleClass('active');
+        $('.header-nav').toggleClass('open');
+        event.preventDefault();
+    });
+    /* When user clicks a link */
+    $('.header-nav li a').click(function () {
+        $('.nav-toggle').toggleClass('active');
+        $('.header-nav').toggleClass('open');
+
+    });
+
+    /***************** Header BG Scroll ******************/
+
+    $(function () {
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 20) {
+                $('section.navigation').addClass('fixed');
+                $('header').css({
+                    "border-bottom": "none",
+                    "padding": "35px 0"
+                });
+                $('header .member-actions').css({
+                    "top": "26px",
+                });
+                $('header .navicon').css({
+                    "top": "34px",
+                });
+            } else {
+                $('section.navigation').removeClass('fixed');
+                $('header').css({
+                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+                    "padding": "50px 0"
+                });
+                $('header .member-actions').css({
+                    "top": "41px",
+                });
+                $('header .navicon').css({
+                    "top": "48px",
+                });
+            }
+        });
+    });
+    /***************** Smooth Scrolling ******************/
+
+    $(function () {
+
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 90
+                    }, 2000);
+                    return false;
+                }
+            }
+        });
+
+    });
+
+    /********************** Social Share buttons ***********************/
+    // var share_bar = document.getElementsByClassName('share-bar');
+    // var po = document.createElement('script');
+    // po.type = 'text/javascript';
+    // po.async = true;
+    // po.src = 'https://apis.google.com/js/platform.js';
+    // var s = document.getElementsByTagName('script')[0];
+    // s.parentNode.insertBefore(po, s);
+
+    // for (var i = 0; i < share_bar.length; i++) {
+    //     var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
+    //         'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
+    //         'style="width:105px; height:21px;">' +
+    //         '</iframe>' +
+
+    //         '<iframe src="//www.facebook.com/plugins/like.php?href=' + encodeURIComponent(window.location) + '&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=true&amp;height=21&amp;appId=101094500229731&amp;width=150" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:150px; height:21px;" allowTransparency="true"></iframe>' +
+
+    //         '<div class="g-plusone" data-size="medium"></div>';
+
+    //     // '<iframe src="https://plusone.google.com/_/+1/fastbutton?bsv&amp;size=medium&amp;url=' + encodeURIComponent(window.location) + '" allowtransparency="true" frameborder="0" scrolling="no" title="+1" style="width:105px; height:21px;"></iframe>';
+
+    //     share_bar[i].innerHTML = html;
+    //     share_bar[i].style.display = 'inline-block';
+    // }
+
+    /********************** Embed youtube video *********************/
+    $('.player').YTPlayer();
+
+
+    /********************** Toggle Map Content **********************/
+    $('#btn-show-map').click(function () {
+        $('#map-content').toggleClass('toggle-map-content');
+        $('#btn-show-content').toggleClass('toggle-map-content');
+    });
+    $('#btn-show-content').click(function () {
+        $('#map-content').toggleClass('toggle-map-content');
+        $('#btn-show-content').toggleClass('toggle-map-content');
+    });
+
+
+    /********************** RSVP **********************/
+    $('#rsvp-form').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        console.log(data);
+
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+            $.post('https://script.google.com/macros/s/AKfycbznqE0NTI8OQVkuQ3ZKkAcl1MBtNi8ua0t3k7-da3Yk30unTNMlW7BP0Q/exec', data)
+                .done(function (data) {
+                    console.log(data);
+                    if (data.result === "error") {
+                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+                    } else {
+                        $('#alert-wrapper').html('');
+                        $('#rsvp-modal').modal('show');
+                    }
+                })
+                .fail(function (data) {
+                    console.log(data);
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                });
+        });
+});
+
+// alert_markup
+function alert_markup(alert_type, msg) {
+    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
+}
